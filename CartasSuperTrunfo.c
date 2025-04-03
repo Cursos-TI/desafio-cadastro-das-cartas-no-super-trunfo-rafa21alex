ad20789f677f24c0e4851f077616b3b2d8198c82
rafa21alex <rafaelalex730@gmail.com>
2025-04-03 20:04:50 -0300

    Update CartasSuperTrunfo.c
    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,8 @@ printf("Super Trunfo! \n");
     int pontos1, pontos2;
     float Area1, Area2;
     float PIB1, PIB2;
+    float densidade1, densidade2;
+    float capita1, capita2;
 
     //entrada de dados da primeira carta
     
@@ -44,12 +46,20 @@ printf("Super Trunfo! \n");
     printf("cidade: %s\n",cidade1);
    
     printf("população: %d\n",populacão1);
-    printf("Área: %f\n",Area1);
+    printf("Área: %.2f\n",Area1);
    
-    printf("PIB: %f\n",PIB1);
+    printf("PIB: %.2f\n",PIB1);
     printf("pontos turisticos: %d\n", pontos1);
 
- // entrada de dados da segunda carta
+    densidade1 = (float)(populacão1 / Area1);
+    
+    printf("densidade populacional: %.2f\n", densidade1);
+
+    capita1 = (float)(populacão1 / PIB1);
+
+    printf("pib per capita: %.2f\n", capita1);
+   
+ // entrada de dados segunda carta
 
     printf("estado:\n");
     scanf("%s", &estado2);
@@ -81,10 +91,18 @@ printf("Super Trunfo! \n");
   printf("cidade: %s\n",cidade2);
 
   printf("população: %d\n",população2);
-  printf("Área: %f\n",Area2);
+  printf("Área: %.2f\n",Area2);
 
-  printf("PIB: %f\n",PIB2);
+  printf("PIB: %.2f\n",PIB2);
   printf("Pontos turisticos: %d\n",pontos2);
+
+  densidade2 = (float)(população2 / Area2);
+
+  printf("Densidade populacional: %.2f\n", densidade2);
+
+  capita2 = (float)(população2 / PIB2);
+
+  printf("PIB per capita: %.2f\n", capita2);
 
   return 0;
 
